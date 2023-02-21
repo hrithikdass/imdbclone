c2c8aaa497b68018c98d6dceafcae2e110e3732d
hrithikdass <hrithikdas@gmail.com>
2023-02-22 00:57:25 +0530

    added header and routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/header/header";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<h1>Hello</h1>} />
+          <Route path="movie/:id" element={<h1>Movie details</h1>} />
+          <Route path="movies/:type" element={<h1>Movies list page</h1>} />
+          <Route path="/*" element={<h1>Error page</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
